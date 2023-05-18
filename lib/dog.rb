2dd601b3e2c2d6181b4920007275809a990dd2e0
Moses <t.content012@gmail.com>
2023-05-18 17:58:38 +0300

    first commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+  def name(name, breed)
+    @name = name
+    @breed = breed
+  end
 
+  # name getter
+  def name
+    @name
+  end
+
+  # name setter
+  def name=(new_name)
+    @name = new_name
+  end
+
+  # breed getter
+  def breed
+    @breed
+  end
+
+  # breed setter
+  def breed=(new_breed)
+    @breed = new_breed
+  end
 end
